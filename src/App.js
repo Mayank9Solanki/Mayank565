3a436f531b9bccb6508917e07d38353cf13a8041
Mayank9Solanki <mayank.scmt@gmail.com>
2023-04-20 14:50:10 +0530

    mayank

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: null,
+    };
+  }
+  componentDidMount() {
+    fetch("https://reqres.in/api/users").then((resp) => {
+      resp.json().then((result) => {
+        // console.warn(result.data);
+        this.setState({users:result.data})
+      })
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Fecth API Data
+        </h1>
+        {
+          this.State.users ?
+          this.State.users.map((item,i)=>
+            <div>
+              <p>{i}--- {item.first_name}
+              </p>
+            </div>
+             )
+          :
+          null
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
