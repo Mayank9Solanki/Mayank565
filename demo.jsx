2a1f7b22a5743ed2fe4eda1058aab7940306b60c
Mayank9Solanki <mayank.scmt@gmail.com>
2023-04-20 11:41:35 +0530

    fniwn

--- demo.jsx
+++ demo.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './demo.css';
 
 const demo = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="App">
+        <h1>
+            Fetch API Data
+        </h1>
+      </div>
+    </>
   );
 };
 
