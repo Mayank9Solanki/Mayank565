c27d95c297142a18f1db9630972a74ed2b36925f
Mayank9Solanki <mayank.scmt@gmail.com>
2023-04-25 15:52:29 +0530

    mayank

--- src/Iphone.jsx
+++ src/Iphone.jsx
@@ -9,21 +9,31 @@ const Iphone = () => {
         return (
           <div className="container" align="center">
             <div className="row">
-              <div className="col-md-4">
+              <div className="col-md-4 mt-5">
                 <div className="card" style={{ width: "18rem" }}>
                   <div className="card-body" style={{ borderRadius: "5px" }}>
-                    <img src={x.thumbnail} className="card-img-top" alt="..." />
-                    <p>ID:{x.id}</p><br/>
-                    <p>DESCRIPITION:{x.description}</p><br/>
-                    <p>PRICE:{x.price}</p><br/><hr/>
-                    <p>discountPercentage:{x.discountPercentage}</p><br/>
-                    <p style={{ color: "red" }}>rating:{x.rating}</p><br/><hr/>
-                    <p style={{ color: "blue" }}>stock:{x.stock}</p><br/>
-                    <p style={{ color: "gold" }}>brand:{x.brand}</p><br/>
-                    <p style={{ color: "black" }}>category:{x.category}</p><br/><hr/>
-                    <div className="card-body">
-                      <h5 className="card-title">{x.title}</h5><br/>
-                    </div>
+                    <img src={x.thumbnail} className="card-img-top" style={{width:"280px",height:"auto"}} />
+                    <p>ID:{x.id}</p>
+                    <br /><br/><hr/>
+                    <p>title:{x.title}</p>
+                    <br /><hr/>
+                    <p>DESCRIPITION:{x.description}</p>
+                    <br /><hr/>
+                    <p>PRICE:{x.price}</p>
+                    <br />
+                    <hr />
+                    <p>discountPercentage:{x.discountPercentage}</p>
+                    <br />
+                    <p style={{ color: "red" }}>rating:{x.rating}</p>
+                    <br />
+                    <hr />
+                    <p style={{ color: "blue" }}>stock:{x.stock}</p>
+                    <br /><hr/>
+                    <p style={{ color: "gold" }}>brand:{x.brand}</p>
+                    <br /><hr/>
+                    <p style={{ color: "black" }}>category:{x.category}</p>
+                    <br />
+                    <hr />
 
                     {/* <img src={x.images} className="card-img-top" alt="..." /> */}
                   </div>
