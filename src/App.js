fa2e5e74d6717f9a1011b9f99dd08f29c55a4135
Mayank9Solanki <mayank.scmt@gmail.com>
2023-04-25 12:10:33 +0530

    mayank

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import React from "react";
-import "./App.css";
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-    };
-  }
-  componentDidMount() {
-    fetch("https://reqres.in/api/users").then((resp) => {
-      resp.json().then((result) => {
-        console.warn(result.data);
-        this.setState({ users: result.data });
-      });
-    });
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>Fecth API Data</h1>
-        {this.state.users
-          ? this.state.users.map((item, i) => (
-              <div>
-                <form>
-                  <p>
-                    No:{i}
-                    Name:{item.first_name}<br/>
-                    Surname:{item.last_name}<br/>
-                    Email:{item.email}<br/>
-                    Image:{item.avatar}<br/>
-                  </p>
-                </form>
-              </div>
-            ))
-          : null}
-      </div>
-    );
-  }
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1>jmj</h1>
+    
+    </>
+  )
 }
 
-export default App;
+export default App
