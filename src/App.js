62df54b92e68ab5f4511336025468b205cce10f7
Mayank9Solanki <mayank.scmt@gmail.com>
2023-04-25 15:18:39 +0530

    mayank

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Iphone from './Iphone'
+import Appcss from './App.css'
 
 
 const App = () => {
--- src/Iphone.jsx
+++ src/Iphone.jsx
@@ -1,45 +1,40 @@
-import React from 'react';
-import {Json} from './Product';
+import React from "react";
+import { JSON } from "./Product";
 
 const Iphone = () => {
-    const Product = Json.Product
+  const products = JSON.products;
   return (
     <>
-      {Product.map(x => {
-          return(
-            <div>
-             <div class="card-group">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
+      {products.map((x) => {
+        return (
+          <div className="container" align="center">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="card" style={{ width: "18rem" }}>
+                  <div className="card-body" style={{ borderRadius: "5px" }}>
+                    <img src={x.thumbnail} className="card-img-top" alt="..." />
+                    <p>ID:{x.id}</p><br/>
+                    <p>DESCRIPITION:{x.description}</p><br/>
+                    <p>PRICE:{x.price}</p><br/><hr/>
+                    <p>discountPercentage:{x.discountPercentage}</p><br/>
+                    <p style={{ color: "red" }}>rating:{x.rating}</p><br/><hr/>
+                    <p style={{ color: "blue" }}>stock:{x.stock}</p><br/>
+                    <p style={{ color: "gold" }}>brand:{x.brand}</p><br/>
+                    <p style={{ color: "black" }}>category:{x.category}</p><br/><hr/>
+                    <div className="card-body">
+                      <h5 className="card-title">{x.title}</h5><br/>
+                    </div>
+
+                    {/* <img src={x.images} className="card-img-top" alt="..." /> */}
+                  </div>
+                </div>
+              </div>
             </div>
-          )
+          </div>
+        );
       })}
     </>
-  )
-}
+  );
+};
 
 export default Iphone;
-
